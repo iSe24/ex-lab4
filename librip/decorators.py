91b78b74848fa7e9a16adaf71eee302035aa6724
iSe24 <ise3201@yahoo.com>
2016-10-27 04:19:33 +0900

    Update decorators.py

--- librip/decorators.py
+++ librip/decorators.py
@@ -35,14 +35,14 @@
 # test_4
 # 1
 # 2
-def print_result(decor):
-    def obertka(*args):
-        print(decor.__name__)
-        if len(args) > 0:
+def print_result(decor):            #decor - то что заворачиваем
+    def obertka(*args):             #ВО ЧТО заворачиваем
+        print(decor.__name__)       #вывод названия функции-decora - по условию
+        if len(args) > 0:           
             d = decor(args[0])
         else:
             d = decor()
-        if (type(d) == list):
+        if (type(d) == list):               
             print("\n".join([str(x) for x in d]))
         elif (type(d) == dict):
             print("\n".join([str(x) + " = " + str(d[x]) for x in d]))
