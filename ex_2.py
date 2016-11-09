749640ee5be9c32f16e042642b78d5399386ce6b
iSe24 <ise3201@yahoo.com>
2016-11-09 21:02:58 +0300

    Add files via upload

--- ex_2.py
+++ ex_2.py
@@ -3,11 +3,11 @@ from librip.gen import gen_random
 from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
-data2 = gen_random(1, 7, 10)
-d2 = ['f','F','fa','d']
+data2 = gen_random(1, 5, 5)
+d2 = ['F','f','fa','d']
 
 print(list(Unique(data1)))
-print(list(data2))
+print(list(Unique(data2)))
 print(list(Unique(d2, ignore_case=True)))
 print(list(Unique(d2, ignore_case=False)))
 #print(list(Unique(d2, ignore_case=False)))
